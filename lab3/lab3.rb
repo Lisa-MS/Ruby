27d193fc8e6c9b7dc69613e957fa2006fdf8669e
Lisa-MS <56061970+Lisa-MS@users.noreply.github.com>
2021-11-19 22:24:08 +0200

    Add files via upload

---
+++ lab3/lab3.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'telegram/bot'
+
+token = '2102535644:AAFL2AMqS_HhRk7T7SY-FTi_WiWJkdV7yGo'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/sitepoint'
+      bot.api.send_message(chat_id: message.chat.id, text: "Welcome to http://sitepoint.com")
+      when '/map'
+      bot.api.send_location(chat_id: message.chat.id, latitude: -37.807416, longitude: 144.985339)
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
